32b99289d30d42cf37fdb3cb5176a74e5e5bd78a
Ouro <155661489+Ouro-dev@users.noreply.github.com>
2024-01-06 12:33:00 -0600

    Add files via upload

---
+++ scripts/autotracking/location_mapping.lua
@@ -0,0 +1,248 @@
+LOCATION_MAPPING = {
+    [0x23804C] = {"@World Map/Forsaken Fortress/Phantom Ganon"},
+    [0x23804F] = {"@World Map/Forsaken Fortress/Chest Guarded by Bokoblin"},
+    [0x238050] = {"@World Map/Forsaken Fortress/Chest on Bed"},
+    [0x23804D] = {"@World Map/Forsaken Fortress/Upper Jail"},
+    [0x23804E] = {"@World Map/Forsaken Fortress/Lower Jail"},
+    [0x238051] = {"@World Map/Forsaken Fortress/Helmaroc King Heart Container"},
+
+    [0x2380B7] = {"@World Map/Star Island/Free Platform"},
+    [0x2380B8] = {"@World Map/Star Island/Cave"},
+
+    [0x23809C] = {"@World Map/Northern Fairy Island/Submarine"},
+    
+    [0x23806C] = {"@World Map/Wind Temple/Two Dirt Patches"},
+    [0x23806D] = {"@World Map/Wind Temple/Tingle Chest"},
+    [0x23806E] = {"@World Map/Wind Temple/Behind Stone Head"},
+    [0x23806F] = {"@World Map/Wind Temple/Left Alcove"},
+    [0x238070] = {"@World Map/Wind Temple/Big Key Chest"},
+    [0x238071] = {"@World Map/Wind Temple/Many Cyclones Room"},
+    [0x238072] = {"@World Map/Wind Temple/Middle of Hub Room"},
+    [0x238073] = {"@World Map/Wind Temple/Spike Room - Free Chest"},
+    [0x238074] = {"@World Map/Wind Temple/Spike Room - Break All Floors"},
+    [0x238075] = {"@World Map/Wind Temple/Wizzrobe Mini-Boss"},
+    [0x238076] = {"@World Map/Wind Temple/Top of Hub Room"},
+    [0x238077] = {"@World Map/Wind Temple/Behind 7 Armor"},
+    [0x238078] = {"@World Map/Wind Temple/Kill Enemies in Basement"},
+    [0x238079] = {"@World Map/Wind Temple/Molgera Heart Container"},
+    
+    [0x2380B0] = {"@World Map/Crescent Moon Island/Chest"},
+    [0x2380B1] = {"@World Map/Crescent Moon Island/Submarine"},
+    
+    [0x2380BD] = {"@World Map/7 Star Isles/Center Platform"},
+    [0x2380BE] = {"@World Map/7 Star Isles/Northern Platform"},
+    [0x2380BF] = {"@World Map/7 Star Isles/Southern Platform"},
+    
+    [0x23808E] = {"@World Map/Overlook Island/Cave"},
+    
+    [0x2380C7] = {"@World Map/4 Eye Reef/Top of Eye"},
+    
+    [0x238052] = {"@World Map/Mother & Child Isles/Interior"},
+    
+    [0x238007] = {"@World Map/Windfall Island/Jail - Tingle Gift 1"},
+    [0x238008] = {"@World Map/Windfall Island/Jail - Tingle Gift 2"},
+    [0x238009] = {"@World Map/Windfall Island/Jail - Maze"},
+    [0x23800F] = {"@World Map/Windfall Island/Green Potion Brewery"},
+    [0x238010] = {"@World Map/Windfall Island/Blue Potion Brewery"},
+    [0x23800A] = {"@World Map/Windfall Island/Lenzo Left Chest"},
+    [0x23800B] = {"@World Map/Windfall Island/Lenzo Right Chest"},
+    [0x23800E] = {"@World Map/Windfall Island/House of Wealth Chest"},
+    [0x238011] = {"@World Map/Windfall Island/Light the Lighthouse"},
+    [0x238012] = {"@World Map/Windfall Island/Transparent Chest"},
+    [0x238013] = {"@World Map/Windfall Island/Pirate Ship Chest"},
+    [0x23800C] = {"@World Map/Windfall Island/Win Sploosh Kaboom 1"},
+    [0x23800D] = {"@World Map/Windfall Island/Win Sploosh Kaboom 2"},
+    
+    [0x238090] = {"@World Map/Pawprint Isle/Chu Cave - Chest"},
+    [0x238091] = {"@World Map/Pawprint Isle/Chu Cave - Left Boulder"},
+    [0x238092] = {"@World Map/Pawprint Isle/Chu Cave - Right Boulder"},
+    [0x238093] = {"@World Map/Pawprint Isle/Chu Cave - Scale Wall"},
+    [0x238094] = {"@World Map/Pawprint Isle/Wizzrobe Cave"},
+    [0x238095] = {"@World Map/Pawprint Isle/Free Platform"},
+    
+    [0x238014] = {"@World Map/Dragon Roost Island/Wind Shrine"},
+    [0x238015] = {"@World Map/Dragon Roost Island/Top of Boulder"},
+    [0x238016] = {"@World Map/Dragon Roost Island/Fly Around Island"},
+    [0x238017] = {"@World Map/Dragon Roost Island/Mail Game"},
+    [0x238018] = {"@World Map/Dragon Roost Island/Secret Cave"},
+    
+    [0x238019] = {"@World Map/Dragon Roost Cavern/First Room"},
+    [0x23801A] = {"@World Map/Dragon Roost Cavern/Water Jug Alcove"},
+    [0x23801B] = {"@World Map/Dragon Roost Cavern/Behind Boards"},
+    [0x23801C] = {"@World Map/Dragon Roost Cavern/Across Lava Pit"},
+    [0x23801D] = {"@World Map/Dragon Roost Cavern/Rat Room"},
+    [0x23801E] = {"@World Map/Dragon Roost Cavern/Rat Room Boarded Chest"},
+    [0x23801F] = {"@World Map/Dragon Roost Cavern/Bird's Nest"},
+    [0x238020] = {"@World Map/Dragon Roost Cavern/Dark Room"},
+    [0x238021] = {"@World Map/Dragon Roost Cavern/Tingle Chest in Hub Room"},
+    [0x238022] = {"@World Map/Dragon Roost Cavern/Pot Room"},
+    [0x238023] = {"@World Map/Dragon Roost Cavern/Mini-Boss"},
+    [0x238024] = {"@World Map/Dragon Roost Cavern/Under Bridge"},
+    [0x238025] = {"@World Map/Dragon Roost Cavern/Tingle Chest in Basement"},
+    [0x238026] = {"@World Map/Dragon Roost Cavern/Big Key Chest"},
+    [0x238027] = {"@World Map/Dragon Roost Cavern/Outside Boss Door Left"},
+    [0x238028] = {"@World Map/Dragon Roost Cavern/Outside Boss Door Right"},
+    [0x238029] = {"@World Map/Dragon Roost Cavern/Gohma Heart Container"},
+    
+    [0x2380B6] = {"@World Map/Flight Control Platform/Submarine"},
+    
+    [0x238099] = {"@World Map/Western Fairy Island/Free Platform"},
+    
+    [0x2380A4] = {"@World Map/Rock Spire Isle/Cave"},
+    [0x2380A5] = {"@World Map/Rock Spire Isle/Beedle's Special Shop - 500 Item"},
+    [0x2380A6] = {"@World Map/Rock Spire Isle/Beedle's Special Shop - 900 Item"},
+    [0x2380A7] = {"@World Map/Rock Spire Isle/Beedle's Special Shop - 950 Item"},
+    [0x2380A8] = {"@World Map/Rock Spire Isle/Western Cannon Platform"},
+    [0x2380A9] = {"@World Map/Rock Spire Isle/Eastern Cannon Platform"},
+    [0x2380AA] = {"@World Map/Rock Spire Isle/Center Platform"},
+    
+    [0x238098] = {"@World Map/Eastern Fairy Island/Cannon Platform"},
+    
+    [0x238053] = {"@World Map/Fire Mountain/Cave"},
+    [0x238054] = {"@World Map/Fire Mountain/Free Platform"},
+    [0x238055] = {"@World Map/Fire Mountain/Cannon Platform"},
+    
+    [0x2380B9] = {"@World Map/Star Belt Archipelago/Free Platform"},
+    
+    [0x2380C6] = {"@World Map/3 Eye Reef/Top of Eye"},
+    
+    [0x23803C] = {"@World Map/Greatfish Isle/Hidden Chest"},
+    
+    [0x2380C1] = {"@World Map/Cyclops Reef/Top of Eye"},
+    [0x2380C2] = {"@World Map/Cyclops Reef/Enemy Platform"},
+    
+    [0x2380CA] = {"@World Map/6 Eye Reef/Top of Eye"},
+    [0x2380CB] = {"@World Map/6 Eye Reef/Cannon Platform"},
+    [0x2380CC] = {"@World Map/6 Eye Reef/Submarine"},
+
+    [0x238040] = {"@World Map/Tower of the Gods/Behind Bombable Wall"},
+    [0x238041] = {"@World Map/Tower of the Gods/Hop Across Boxes"},
+    [0x23803D] = {"@World Map/Tower of the Gods/Light Torches"},
+    [0x23803E] = {"@World Map/Tower of the Gods/Skull Room"},
+    [0x23803F] = {"@World Map/Tower of the Gods/Skull Room Shoot the Eye"},
+    [0x238042] = {"@World Map/Tower of the Gods/Tingle Chest"},
+    [0x238043] = {"@World Map/Tower of the Gods/First Chest Guarded"},
+    [0x238044] = {"@World Map/Tower of the Gods/Stone Tablet"},
+    [0x238045] = {"@World Map/Tower of the Gods/Darknut Mini-Boss"},
+    [0x238046] = {"@World Map/Tower of the Gods/Second Chest Guarded"},
+    [0x238047] = {"@World Map/Tower of the Gods/Flying Platforms 1"},
+    [0x238048] = {"@World Map/Tower of the Gods/Flying Platforms 2"},
+    [0x238049] = {"@World Map/Tower of the Gods/Big Key Chest"},
+    [0x23804A] = {"@World Map/Tower of the Gods/Gohdan Heart Container"},
+    
+    [0x23804B] = {"@World Map/Hyrule/Master Sword Room"},
+    [0x23807A] = {"@World Map/Hyrule/Ganon's Tower - Maze Chest"},
+    
+    [0x238096] = {"@World Map/Thorned Fairy Island/Cannon Platform"},
+    [0x238097] = {"@World Map/Thorned Fairy Island/Enemy Platform"},
+    
+    [0x238082] = {"@World Map/Needle Rock Isle/Chest in Ring of Fire"},
+    [0x238083] = {"@World Map/Needle Rock Isle/Cave"},
+    
+    [0x23808C] = {"@World Map/Islet of Steel/Cave"},
+    [0x23808D] = {"@World Map/Islet of Steel/Enemy Platform"},
+    
+    [0x238089] = {"@World Map/Stone Watcher Island/Cave"},
+    [0x23808A] = {"@World Map/Stone Watcher Island/Free Platform"},
+    [0x23808B] = {"@World Map/Stone Watcher Island/Cannon Platform"},
+    
+    [0x23807E] = {"@World Map/Private Oasis/Top of Waterfall"},
+    [0x23807F] = {"@World Map/Private Oasis/Cabana Labyrinth - Lower"},
+    [0x238080] = {"@World Map/Private Oasis/Cabana Labyrinth - Upper"},
+    
+    [0x2380A1] = {"@World Map/Bomb Island/Cave"},
+    [0x2380A2] = {"@World Map/Bomb Island/Free Platform"},
+    [0x2380A3] = {"@World Map/Bomb Island/Submarine"},
+    
+    [0x23808F] = {"@World Map/Bird's Peak Rock/Cave"},
+    
+    [0x23809E] = {"@World Map/Diamond Steppe Island/Warp Maze First Chest"},
+    [0x23809F] = {"@World Map/Diamond Steppe Island/Warp Maze Second Chest"},
+    
+    [0x2380C8] = {"@World Map/5 Eye Reef/Top of Eye"},
+    [0x2380C9] = {"@World Map/5 Eye Reef/Free Platform"},
+    
+    [0x2380AC] = {"@World Map/Shark Island/Cave"},
+    
+    [0x23809A] = {"@World Map/Southern Fairy Island/NW Cannon Platform"},
+    [0x23809B] = {"@World Map/Southern Fairy Island/SE Cannon Platform"},
+    
+    [0x238057] = {"@World Map/Ice Ring Isle/Frozen Chest"},
+    [0x238058] = {"@World Map/Ice Ring Isle/Cave"},
+    [0x238059] = {"@World Map/Ice Ring Isle/Inner Cave"},
+    
+    [0x23802A] = {"@World Map/Forest Haven/On Tree Branch"},
+    [0x23802B] = {"@World Map/Forest Haven/On Small Island"},
+    
+    [0x23802C] = {"@World Map/Forbidden Woods/First Chest"},
+    [0x23802D] = {"@World Map/Forbidden Woods/In Bottom Tree"},
+    [0x23802E] = {"@World Map/Forbidden Woods/Climb to Top"},
+    [0x23802F] = {"@World Map/Forbidden Woods/Hole in Tree"},
+    [0x238030] = {"@World Map/Forbidden Woods/In Morth's Pit"},
+    [0x238031] = {"@World Map/Forbidden Woods/Vine Maze Left"},
+    [0x238032] = {"@World Map/Forbidden Woods/Vine Maze Right"},
+    [0x238033] = {"@World Map/Forbidden Woods/Tall Room Before Mini-Boss"},
+    [0x238034] = {"@World Map/Forbidden Woods/Mothula Mini-Boss Room"},
+    [0x238035] = {"@World Map/Forbidden Woods/Past Hanging Vines"},
+    [0x238036] = {"@World Map/Forbidden Woods/Across the Flower"},
+    [0x238037] = {"@World Map/Forbidden Woods/Tingle Chest"},
+    [0x238038] = {"@World Map/Forbidden Woods/Locked in Basement Trunk"},
+    [0x238039] = {"@World Map/Forbidden Woods/Big Key Chest"},
+    [0x23803A] = {"@World Map/Forbidden Woods/Double Mothula"},
+    [0x23803B] = {"@World Map/Forbidden Woods/Kalle Demos Heart Container"},
+    
+    [0x2380AD] = {"@World Map/Cliff Plateau Isles/Cave"},
+    [0x2380AE] = {"@World Map/Cliff Plateau Isles/Upper"},
+    [0x2380AF] = {"@World Map/Cliff Plateau Isles/Free Platform"},
+    
+    [0x2380B2] = {"@World Map/Horseshoe Island/Play Golf"},
+    [0x2380B3] = {"@World Map/Horseshoe Island/Cave"},
+    [0x2380B4] = {"@World Map/Horseshoe Island/NW Free Platform"},
+    [0x2380B5] = {"@World Map/Horseshoe Island/SE Free Platform"},
+    
+    [0x238000] = {"@World Map/Outset Island/Under Link's House"},
+    [0x238001] = {"@World Map/Outset Island/Grasscutter House"},
+    [0x238006] = {"@World Map/Outset Island/Give Orca 10 Knight's Crests"},
+    [0x238002] = {"@World Map/Outset Island/Jabun's Cave"},
+    [0x238003] = {"@World Map/Outset Island/Big Pig"},
+    [0x238004] = {"@World Map/Outset Island/Savage Labyrinth Floor 30"},
+    [0x238005] = {"@World Map/Outset Island/Savage Labyrinth Floor 50"},
+
+    [0x23807B] = {"@World Map/Great Sea/Cyclos"},
+    [0x23807C] = {"@World Map/Great Sea/Withered Trees"},
+    [0x23807D] = {"@World Map/Great Sea/Ghost Ship"},
+    
+    [0x23805A] = {"@World Map/Headstone Island/Top of Island"},
+    [0x23805B] = {"@World Map/Headstone Island/Submarine"},
+    
+    [0x23805C] = {"@World Map/Earth Temple/First Room"},
+    [0x23805E] = {"@World Map/Earth Temple/Behind Destructible Walls"},
+    [0x23805D] = {"@World Map/Earth Temple/Transparent Chest in 1st Crypt"},
+    [0x23805F] = {"@World Map/Earth Temple/3 Blocks Room"},
+    [0x238060] = {"@World Map/Earth Temple/Behind Statues"},
+    [0x238061] = {"@World Map/Earth Temple/Casket in 2nd Crypt"},
+    [0x238062] = {"@World Map/Earth Temple/Stalfos Mini-Boss"},
+    [0x238063] = {"@World Map/Earth Temple/Tingle Chest"},
+    [0x238064] = {"@World Map/Earth Temple/Floormaster Room Free"},
+    [0x238065] = {"@World Map/Earth Temple/Floormaster Room Fight"},
+    [0x238066] = {"@World Map/Earth Temple/Chest in 3rd Crypt"},
+    [0x238067] = {"@World Map/Earth Temple/Many Mirrors Left"},
+    [0x238068] = {"@World Map/Earth Temple/Many Mirrors Right"},
+    [0x238069] = {"@World Map/Earth Temple/Stalfos Crypt Room"},
+    [0x23806A] = {"@World Map/Earth Temple/Big Key Chest"},
+    [0x23806B] = {"@World Map/Earth Temple/Jalhalla Heart Container"},
+    
+    [0x2380C3] = {"@World Map/2 Eye Reef/Top of Eye"},
+    [0x2380C4] = {"@World Map/2 Eye Reef/Free Platform"},
+    [0x2380C5] = {"@World Map/2 Eye Reef/Big Octo Fairy"},
+
+    [0x238085] = {"@World Map/Angular Isles/Peak"},
+    [0x238086] = {"@World Map/Angular Isles/Cave"},
+    
+    [0x238087] = {"@World Map/Boating Course/Raft"},
+    [0x238088] = {"@World Map/Boating Course/Cave"},
+    
+    [0x2380BA] = {"@World Map/5 Star Isles/Raft"},
+    [0x2380BB] = {"@World Map/5 Star Isles/Submarine"},
+    [0x2380BC] = {"@World Map/5 Star Isles/Cannon Platform"},
+}
