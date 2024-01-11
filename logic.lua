3b263086f7d26c54e7842af100e42cccad558947
Ouro <155661489+Ouro-dev@users.noreply.github.com>
2024-01-10 18:53:32 -0600

    Add files via upload

---
+++ logic.lua
@@ -0,0 +1,15 @@
+function fw_access()
+    return (has("grapple") and has("leaf") and has("windwaker") and has("requiem"))
+end
+
+function totg_access()
+    return (has("din") and has("farore") and has("nayru"))
+end
+
+function et_access()
+    return (has("power") and has("windwaker") and has("melody"))
+end
+
+function wt_access()
+    return (has("boots") and has("hammer") and has("windwaker") and has("melody"))
+end
